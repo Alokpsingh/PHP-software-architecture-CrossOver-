6af3018097f462afb12b79ebdd36b2256ed03bf0
Alok P. Singh <alok.submit@gmail.com>
2018-10-10 15:13:34 +0530

    Fix Panel Serial Size to 16 length

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Admin Panel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        @yield('styles')
 
         <!-- Styles -->
         <style>
@@ -63,6 +65,7 @@
                 margin-bottom: 30px;
             }
         </style>
+
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -79,17 +82,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    One Day Electricities
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @include("Widgets::LineChartView")
             </div>
         </div>
     </body>
+    @yield('scripts')
+
 </html>
